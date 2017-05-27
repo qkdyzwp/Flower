f066bf55e74433157808a304e5e8c8203e32fca9
qkdyzwp <446893043@qq.com>
2017-05-27 16:46:36 +0800

    加入下拉刷新基类

--- app/src/main/java/com/fir/open/sorce/fragment/HomeFragemnt2.java
+++ app/src/main/java/com/fir/open/sorce/fragment/HomeFragemnt2.java
@@ -1,18 +1,11 @@
 package com.fir.open.sorce.fragment;
 
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
+
 import com.fir.open.sorce.R;
-import com.fir.open.sorce.activity.TestActivityForPull;
 import com.fir.open.sorce.adapter.PullBaseAdapter;
-import com.fir.open.sorce.fragment.base.BaseFragment;
-import com.fir.open.sorce.fragment.base.PullBaseFragment;
 import com.fir.open.sorce.fragment.base.ScrPullBaseFragment;
-import com.fir.open.sorce.inter.PullData;
 import com.fir.open.sorce.inter.ScrPullData;
 import com.fir.open.sorce.manager.FullyLinearLayoutManager;
 
@@ -23,7 +16,7 @@ import java.util.List;
  * Created by mododata-android on 2017/5/25.
  */
 
-public class HomeFragemnt extends ScrPullBaseFragment {
+public class HomeFragemnt2 extends ScrPullBaseFragment {
     private View view;
     private RecyclerView base_pull_list_view;
 
